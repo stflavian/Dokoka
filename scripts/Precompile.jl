b30551f1da4e9321a3ef0edb6d6783b3516db316
Flavian Stavarache <f.stavarache@student.tue.nl>
2025-01-22 16:26:38 +0100

    remove dependance on .xyz file and clean directory after precompilation

--- scripts/Precompile.jl
+++ scripts/Precompile.jl
@@ -1,8 +1,39 @@
 using Dokoka
 
-Dokoka.parseargs(false)
-#Dokoka.goradial!(Dokoka.readmol("../benzene.xyz"), Dokoka.readmol("../benzene.xyz"), [0.0, 5.0, 0.0], [1.0, 0.0, 0.0], 0.0, 1)
-#Dokoka.gorotational!(molecule1, molecule2, [0.0, 5.0, 0.0], [1.0, 0.0, 0.0], 0.0, 1)
-#Dokoka.goangular!(molecule1, molecule2, [0.0, 5.0, 0.0], [1.0, 0.0, 0.0], 0.0, 1)
-#Dokoka.gorandom!(molecule1, molecule2, [0.0, 5.0, 0.0], [1.0, 0.0, 0.0], 0.0, 1)
+file = open("mock-build.xyz", "w")
+write(file, 
+"""12
+Benzene
+H      1.2194     -0.1652      2.1600
+C      0.6825     -0.0924      1.2087
+C     -0.7075     -0.0352      1.1973
+H     -1.2644     -0.0630      2.1393
+C     -1.3898      0.0572     -0.0114
+H     -2.4836      0.1021     -0.0204
+C     -0.6824      0.0925     -1.2088
+H     -1.2194      0.1652     -2.1599
+C      0.7075      0.0352     -1.1973
+H      1.2641      0.0628     -2.1395
+C      1.3899     -0.0572      0.0114
+H      2.4836     -0.1022      0.0205
+""")
+close(file)
 
+Dokoka.parseargs(false)
+
+molecule1 = Dokoka.readmol("mock-build.xyz") 
+molecule2 = Dokoka.readmol("mock-build.xyz")
+
+Dokoka.goradial!(molecule1, molecule2, [0.0, 5.0, 0.0], [1.0, 0.0, 0.0], 0.0, 1)
+rm("radial.xyz")
+
+Dokoka.gorotational!(molecule1, molecule2, [0.0, 5.0, 0.0], [1.0, 0.0, 0.0], 0.0, 1)
+rm("rotational.xyz")
+
+Dokoka.goangular!(molecule1, molecule2, [0.0, 5.0, 0.0], [1.0, 0.0, 0.0], 0.0, 1)
+rm("angular.xyz")
+
+Dokoka.gorandom!(molecule1, molecule2, [0.0, 5.0, 0.0], [1.0, 0.0, 0.0], 0.0, 1)
+rm("random.xyz")
+
+rm("mock-build.xyz")
