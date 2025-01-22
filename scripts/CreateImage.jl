d207244585e58222c5f22de0c6fae737dc6262b5
Flavian Stavarache <f.stavarache@student.tue.nl>
2025-01-22 16:28:03 +0100

    automatically detect the distro during compilation

--- scripts/CreateImage.jl
+++ scripts/CreateImage.jl
@@ -4,4 +4,15 @@ using Pkg
 Pkg.activate(".")
 Pkg.instantiate()
 
-create_app("Dokoka", "../build", filter_stdlibs=true, precompile_execution_file="scripts/precompile.jl")
+if !Sys.islinux()
+    "The code can only be compiled on Linux for now. Exiting..."
+    exit()
+end
+
+release_file = open("/etc/os-release", "r")
+_ = readuntil(release_file, "\nID=")
+distro_name = readline(release_file)
+close(release_file)
+
+create_app(".", "build/$distro_name", precompile_execution_file="scripts/Precompile.jl",
+            executables=["dokoka" => "julia_main"])
